201db9efd84a7f3cad33e21408ae125b57a8e782
Kieran Collins <kiecol@protonmail.com>
2023-07-19 15:48:00 +0000

    Add product admin functions to adjust if products have sizes

--- products/admin.py
+++ products/admin.py
@@ -10,9 +10,20 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
+        'has_sizes',
     )
 
     ordering = ('sku',)
+
+    actions = ['products_have_sizes', 'products_dont_have_sizes']
+
+    def products_have_sizes(self, request, queryset):
+        queryset.update(has_sizes=True)
+        self.message_user(request, "Sizes have been added to the selected products.")
+
+    def products_dont_have_sizes(self, request, queryset):
+        queryset.update(has_sizes=False)
+        self.message_user(request, "Sizes have been removed from the selected products.")
 
 
 class CategoryAdmin(admin.ModelAdmin):
